550c46f5d52d52f6a624cd94152870f425e4ea8a
Peter Hyatt <peter.hyatt@gmail.com>
2023-05-03 20:13:02 -0400

    First pass on setting up form and packages.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,156 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+type Inputs = {
+  vlookupColumn: string;
+  inputColumn: string;
+  threshold: number;
+  rows: number;
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // const [rows, setRows] = useState(4);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  const rows = watch("rows", 4);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        
+        {/* register your input into the hook by invoking the "register" function */}
+        <div className="m-3 prose">
+          Using <a href="https://github.com/nol13/fuzzball.js">fuzzball.js</a>{" "}
+          aka{" "}
+          <a href="https://en.wikipedia.org/wiki/Levenshtein_distance">
+            Levenshtein Distance
+          </a>{" "}
+          calculation to match up values.
         </div>
-      </div>
+        <div className="flex">
+          <div className="m-3">
+            <label
+              htmlFor="vlookup-column"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Lookup Column
+            </label>
+            <textarea
+              id="vlookup-column"
+              placeholder="Lookup Column"
+              rows={rows || 4}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              {...register("vlookupColumn")}
+            />
+            {errors["vlookupColumn"] && <span>This field is required</span>}
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+          {/* include validation with required or other standard HTML validation rules */}
+          <div className="m-3">
+            <label
+              htmlFor="input-column"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Input Column
+            </label>
+            <textarea
+              id="input-column"
+              placeholder="Input Column"
+              rows={rows || 4}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              {...register("inputColumn", { required: true })}
+            />
+            {/* errors will return when field validation fails  */}
+            {errors["inputColumn"] && <span>This field is required</span>}
+          </div>
+
+          <div className="m-3">
+            <label
+              htmlFor="input-column"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Output Column
+            </label>
+            <textarea
+              id="output-column"
+              placeholder="output Column"
+              rows={rows || 4}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              disabled
+              // {...register("inputColumn", { required: true })}
+            />
+            {/* errors will return when field validation fails  */}
+            {errors["inputColumn"] && <span>This field is required</span>}
+          </div>
+
+          <div className="p-2">
+            <span className="mb-4">
+              <label
+                htmlFor="threshold"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Threshold
+              </label>
+              <input
+                type="number"
+                id="threshold"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="90"
+                defaultValue={90}
+                required
+                {...register("threshold", { required: true })}
+              />
+            </span>
+
+            <span className="mb-3">
+              <label
+                htmlFor="rows"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Rows
+              </label>
+              <input
+                type="number"
+                id="rows"
+                placeholder="4 (Optional)"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+                {...register("rows")}
+              />
+            </span>
+          </div>
+
+          <button
+            type="submit"
+            className="m-3 mt-10 h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Go
+          </button>
+        </div>
+      </form>
+      <div className="relative flex place-items-center">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+          src="/apple-touch-icon.png"
+          alt="Fuzzy Bear"
+          width={50}
           height={37}
           priority
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
