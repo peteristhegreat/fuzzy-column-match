7cead3f5fd9109c3c359b88248ee12089cf6a6c5
Peter Hyatt <peter.hyatt@gmail.com>
2023-05-04 23:08:39 -0400

    Add clear button. Add random input button. Fix wording a bit.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useEffect, useState } from "react";
 import { generateOutputColumn } from "@/utils/fuzzySearch";
-import { range } from "lodash";
+import { last, random, range } from "lodash";
 
 type Inputs = {
   vlookupColumn: string;
@@ -12,16 +12,50 @@ type Inputs = {
   rows: number;
 };
 
+const inputSets = [
+  {
+    vlookupColumn: "asdf\nqwer\nzxcvb\nyuio",
+    inputColumn: "asd\nwer\ncxbz\nyou",
+    threshold: 50,
+    rows: 4,
+  },
+  {
+    vlookupColumn: "The quick brown fox jumped over the lazy dog."
+      .split(" ")
+      .join("\n"),
+    inputColumn: "thE misspelled word jumps oevr hazy browser."
+      .split(" ")
+      .join("\n"),
+    threshold: 80,
+    rows: 9,
+  },
+  {
+    vlookupColumn:
+      "Company 1;Test Company;New Client;Weird One;The Best LLC;John Smith"
+        .split(";")
+        .join("\n"),
+    inputColumn:
+      "Smith, John;J. Smith;Smith, J.;Jose Smit;1 Company;Smithe;Old Client;Best;Test Co."
+        .split(";")
+        .join("\n"),
+    threshold: 90,
+    rows: 9,
+  },
+];
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [outputColumn, setOutputColumn] = useState("");
   const [closeMatches, setCloseMatches] = useState("");
+  const [rowCounts, setRowCounts] = useState([0, 0, 0, 0]);
+  const [lastInputSet, setLastInputSet] = useState(-1);
 
   const {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
@@ -30,16 +64,14 @@ export default function Home() {
     // @ts-ignore
     const threshold = parseInt(data.threshold);
 
-    const [oc, cm] = generateOutputColumn(
-      data.vlookupColumn.trim().split("\n"),
-      data.inputColumn.trim().split("\n"),
-      threshold
-    );
+    const luc = data.vlookupColumn.trim().split("\n");
+    const ic = data.inputColumn.trim().split("\n");
+    const [oc, cm] = generateOutputColumn(luc, ic, threshold);
     console.log({ oc, cm });
     setOutputColumn("1234");
-    setCloseMatches("57689")
+    setCloseMatches("57689");
     setOutputColumn(oc.join("\n"));
-    const sortedMatches : string[] = [];
+    const sortedMatches: string[] = [];
     range(threshold, Math.min(60, Math.max(threshold - 10, 1)), -1).forEach(
       (val) => {
         if (cm[val]) {
@@ -51,6 +83,8 @@ export default function Home() {
       }
     );
     setCloseMatches(sortedMatches.join("\n"));
+
+    setRowCounts([luc.length, ic.length, oc.length, sortedMatches.length]);
   };
 
   const rows = watch("rows", 4);
@@ -63,13 +97,18 @@ export default function Home() {
         {/* register your input into the hook by invoking the "register" function */}
         <div className="m-3 prose">
           <h1>Fuzzy Column Match</h1>
-          <p>This uses <a href="https://github.com/nol13/fuzzball.js">fuzzball.js</a>{" "}
-          aka{" "}
-          <a href="https://en.wikipedia.org/wiki/Levenshtein_distance">
-            Levenshtein Distance
-          </a>{" "}
-          calculations to match up values.</p>
-          <p>The input column will be one to one to the output column in row count.</p>
+          <p>
+            This uses{" "}
+            <a href="https://github.com/nol13/fuzzball.js">fuzzball.js</a> aka{" "}
+            <a href="https://en.wikipedia.org/wiki/Levenshtein_distance">
+              Levenshtein Distance
+            </a>{" "}
+            calculations to match up values.
+          </p>
+          <p>
+            The input column will be one-to-one to the output column in row
+            count.
+          </p>
         </div>
         <div className="flex">
           <div className="m-3">
@@ -77,7 +116,7 @@ export default function Home() {
               htmlFor="vlookup-column"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Lookup Column
+              Lookup Column{rowCounts[0] ? <> ({rowCounts[0]})</> : ""}
             </label>
             <textarea
               id="vlookup-column"
@@ -95,7 +134,7 @@ export default function Home() {
               htmlFor="input-column"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Input Column
+              Input Column{rowCounts[1] ? <> ({rowCounts[1]})</> : ""}
             </label>
             <textarea
               id="input-column"
@@ -113,11 +152,11 @@ export default function Home() {
               htmlFor="input-column"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Output Column
+              Output Column{rowCounts[2] ? <> ({rowCounts[2]})</> : ""}
             </label>
             <textarea
               id="output-column"
-              placeholder="output Column"
+              placeholder="Output Column"
               rows={rows || 4}
               className="whitespace-pre block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               value={outputColumn}
@@ -138,6 +177,8 @@ export default function Home() {
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="90"
                 defaultValue={90}
+                min={0}
+                max={100}
                 required
                 {...register("threshold", { required: true })}
               />
@@ -154,31 +195,72 @@ export default function Home() {
                 type="number"
                 id="rows"
                 placeholder="4 (Optional)"
+                min={2}
+                max={10000}
+                // step={5}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 {...register("rows")}
               />
             </span>
           </div>
-
+        </div>
+        <div className="ml-auto float-right">
           <button
             type="submit"
-            className="m-3 mt-10 h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="mr-3 mt-10 h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
             Go
           </button>
+
+          <button
+            type="button"
+            className="mr-3 mt-10 h-12 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => {
+              reset({
+                vlookupColumn: "",
+                inputColumn: "",
+                rows: 4,
+                threshold: 90,
+              });
+              setCloseMatches("");
+              setOutputColumn("");
+              setRowCounts([0, 0, 0, 0]);
+            }}
+          >
+            Clear
+          </button>
+
+          <button
+            type="button"
+            className="mr-3 mt-10 h-12 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => {
+              let idx = lastInputSet + 1;
+              if (lastInputSet == -1) {
+                const idx = random(inputSets.length);
+              }
+              setLastInputSet(idx % inputSets.length);
+              reset(inputSets[idx % inputSets.length]);
+              setCloseMatches("");
+              setOutputColumn("");
+              setRowCounts([0, 0, 0, 0]);
+            }}
+          >
+            Random Sample Data
+          </button>
         </div>
+        <div className="clear-both"></div>
 
         <div className="m-3">
           <label
             htmlFor="close-matches"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Closest Matches
+            Closest Matches{rowCounts[3] ? <> ({rowCounts[3]})</> : ""}
           </label>
           <textarea
             id="close-matches"
             placeholder="Values nearly matching under the threshold"
-            rows={rows || 4}
+            rows={Math.max(closeMatches.split("\n").length, 4)}
             className="whitespace-pre block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             value={closeMatches}
           />
