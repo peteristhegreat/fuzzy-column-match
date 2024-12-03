1475c2ed38f26e5f54102fe81c44ea90acd3503f
Peter Hyatt <peter.hyatt@gmail.com>
2024-12-02 22:47:38 -0500

    Add manifest and head.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,63 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+          <Head >
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Fuzzy Column Match" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>Fuzzy Column Match</title>
+
+
+        <meta name="application-name" content="Fuzzy Column Match" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Fuzzy Column Match" />
+        <meta name="description" content="Best Fuzzy Column Match in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        {/* <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" /> */}
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        {/* <link rel="manifest" href="/site.manifest.json" /> */}
+        {/* <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" /> */}
+        {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" /> */}
+
+        {/* <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="Fuzzy Column Match" />
+        <meta name="twitter:description" content="Best Fuzzy Column Match in the world" />
+        <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
+        <meta name="twitter:creator" content="@DavidWShadow" /> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Fuzzy Column Match" />
+        <meta property="og:description" content="Best Fuzzy Column Match in the world" />
+        <meta property="og:site_name" content="Fuzzy Column Match" />
+        <meta property="og:url" content="https://fuzzy-column-match.vercel.app" />
+        <meta property="og:image" content="https://fuzzy-column-match.vercel.app/apple-touch-icon.png" />
+
+
+        {/* <!-- apple splash screen images -->
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
